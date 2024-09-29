38ef893f86068bebcde7af089f5facbb58f71cbf
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-09-29 11:07:39 +0530

    server

--- server/index.js
+++ server/index.js
@@ -5,9 +5,35 @@ const app = express()
 
 app.use(cors())
 
-app.get('/',(req,res) => {
-   res.send("<h1>Hello World </h1>")
-})
+// app.get('/',(req,res) => {
+//    res.send("<h1>Hello World </h1>")
+// })
+
+app.get('/', (req, res) => {
+
+    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385043&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+  
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('An error occurred');
+      });
+  });
 
 app.listen(8080,() => {
     console.log("server on 8080")
