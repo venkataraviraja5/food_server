a775082252a66358db6780b0b2f1da1cfd3de64e
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-09-29 13:02:38 +0530

    server

--- server/index.js
+++ server/index.js
@@ -33,6 +33,36 @@ app.get('/', (req, res) => {
         console.error(error);
         res.status(500).send('An error occurred');
       });
+});
+
+app.get('/menu/:id', (req, res) => {
+
+    const {id} = req.params
+
+   // console.log(id)
+
+    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.1685786&lng=79.9338798&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
+  
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('An error occurred');
+      });
   });
 
 app.listen(8080,() => {
